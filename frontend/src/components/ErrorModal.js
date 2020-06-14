eea56ad6e46f648ae5b343ccc33da4598b20cf56
Ryan <44525616+Ryan-Oneil@users.noreply.github.com>
2020-06-14 19:53:50 +0100

    Changed errorModal

--- frontend/src/components/ErrorModal.js
+++ frontend/src/components/ErrorModal.js
@@ -1,8 +1,32 @@
-import { Modal } from "antd";
+import { Button, Modal } from "antd";
+import React from "react";
+import { connect } from "react-redux";
+import { clearError, disableError } from "../actions/errors";
 
-export default message => {
-  Modal.error({
-    title: "An error has occurred",
-    content: message
-  });
+const ErrorModal = props => {
+  const { message } = props;
+
+  const closeModal = () => {
+    props.clearError();
+  };
+
+  return (
+    <Modal
+      visible
+      onCancel={closeModal}
+      onOk={closeModal}
+      title="An Error Occurred"
+      footer={[
+        <Button key="disable" onClick={() => props.disableError()}>
+          Disable
+        </Button>,
+        <Button key="confirm" type="primary" onClick={() => props.clearError()}>
+          Ok
+        </Button>
+      ]}
+    >
+      <p>{message}</p>
+    </Modal>
+  );
 };
+export default connect(null, { clearError, disableError })(ErrorModal);
